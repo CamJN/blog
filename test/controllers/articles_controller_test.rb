e6b519d173d79f3e45004abf5860d1e33b41a756
Camden Narzt <camden.narzt@metabolistics.com>
2014-07-11 15:15:21 -0600

    start testing article controller

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:articles)
+  end
+
+  test "should create article when admin makes request" do
+    sign_in users(:ted)
+
+    #assert_equal 'ted', current_user.username
+
+    assert_difference('Article.count') do
+      post :create, article: {title: 'Some title', body: 'a body'}
+    end
+
+    assert_redirected_to article_path(assigns(:article))
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/pride"
+
+
+# Add more helper methods to be used by all tests in the following classes
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionController::TestCase
+  include Devise::TestHelpers
 end
