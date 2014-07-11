811f64ab5b8a9a73e14d1d9e9fee9a26a74002e5
Camden Narzt <camden.narzt@metabolistics.com>
2014-07-11 10:30:05 -0600

    Speed up tests by validating not saving

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -2,26 +2,24 @@ require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
 
-  test "article should not save without title" do
+  test "article should not be valid without title" do
     notitle = Article.new
     notitle.text = 'acceptable'
-
-    assert_not notitle.save, "Saved the article without a title"
+    assert_not notitle.valid?, "validated the article without a title"
   end
 
-  test "article should not save with too short of a title" do
+  test "article should not be valid with too short of a title" do
     notitle = Article.new
     notitle.text = 'acceptable'
     notitle.title = 'shrt'
-
-    assert_not notitle.save, "Saved the article with a short title"
+    assert_not notitle.valid?, "validated the article with a short title"
   end
 
-  test "article should not save with too long of a title" do
+  test "article should not be valid with too long of a title" do
     notitle = Article.new
     notitle.text = 'acceptable'
     notitle.title = 'thistitleistoolongtofitinthedatabasefieldthatholdsarticletitlesthistitleistoolongtofitinthedatabasefieldthatholdsarticletitlesthistitleistoolongtofitinthedatabasefieldthatholdsarticletitlesthistitleistoolongtofitinthedatabasefieldthatholdsarticletitlesseethat'
-    assert_not notitle.save, "Saved the article with a long title"
+    assert_not notitle.valid?, "validated the article with a long title"
   end
 
   test "article's title setter should title-case the title" do
@@ -30,38 +28,37 @@ class ArticleTest < ActiveSupport::TestCase
     assert_equal 'This Title Is Too Lowercase', notitle.title, "Didn't title-case the title when it was set"
   end
 
-  test "article with acceptable title should save" do
+  test "article with acceptable title should be valid" do
     good = Article.new
     good.title = 'acceptable'
     good.text = 'acceptable'
-    assert good.save, "Didn't save the article with a valid title"
+    assert good.valid?, "Didn't valid the article with a valid title"
   end
 
-  test "article with duplicate title should not save" do
+  test "article with duplicate title should not be valid" do
     duptitle = Article.new
     duptitle.text = 'acceptable'
     duptitle.title = articles(:good).title
-    assert_equal duptitle.title, articles(:good).title, "Test incorrect, titles not equal"
-    assert_not duptitle.save, "Saved article with a duplicate title"
+    assert_not duptitle.valid?, "validated article with a duplicate title"
   end
 
-  test "article should not save without text" do
+  test "article should not be valid without text" do
     notext = Article.new
     notext.title = 'acceptable'
-    assert_not notext.save, "Saved the article without text"
+    assert_not notext.valid?, "validated the article without text"
   end
 
-  test "article should not save with degenerate string text" do
+  test "article should not be valid with degenerate string text" do
     notext = Article.new
     notext.title = 'acceptable'
     notext.text = ''
-    assert_not notext.save, "Saved the article with degenerate string text"
+    assert_not notext.valid?, "validated the article with degenerate string text"
   end
 
-  test "article with acceptable text should save" do
+  test "article with acceptable text should valid" do
     good = Article.new
     good.title = 'acceptable'
     good.text = 'acceptable'
-    assert good.save, "Didn't save the article with valid text"
+    assert good.valid?, "Didn't valid the article with valid text"
   end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
 
-  test "comment should not save without body" do
+  test "comment should not be valid without body" do
     nobody = Comment.new
     nobody.user_id = users(:bob).id
     nobody.article_id = articles(:good).id
-    assert_not nobody.save, "Comment saved with no body"
+    assert_not nobody.valid?, "Comment valid with no body"
   end
 
-  test "comment should not save with short body" do
+  test "comment should not be valid with short body" do
     shortbody = Comment.new
     shortbody.user_id = users(:bob).id
     shortbody.article_id = articles(:good).id
     shortbody.body = 'short'
-    assert_not shortbody.save, "Comment saved with no body"
+    assert_not shortbody.valid?, "Comment valid with no body"
   end
 
-  test "comment should not save with duplicate body" do
+  test "comment should not be valid with duplicate body" do
     dupbody = Comment.new
     dupbody.user_id = users(:bob).id
     dupbody.article_id = articles(:good).id
     dupbody.body = comments(:one).body
-    assert_not dupbody.save, "Comment saved with duplicate body"
+    assert_not dupbody.valid?, "Comment valid with duplicate body"
   end
 
 end
