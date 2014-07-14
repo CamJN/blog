a9caa15944ed22328e554ac0c76164295342e64a
Camden Narzt <camden.narzt@metabolistics.com>
2014-07-14 16:48:45 -0600

    Started testing comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
+    params['comment']['user_id'] = current_user.id
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
@@ -16,6 +17,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:commenter,:body)
+    params.require(:comment).permit(:body,:user_id)
   end
 end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,4 +4,76 @@ class CommentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "admin can create comments" do
+    flunk "unimplemented"
+  end
+
+  test "moderator can create comments" do
+    flunk "unimplemented"
+  end
+
+  test "registered can create comments" do
+    flunk "unimplemented"
+  end
+
+  test "guest cannot create comments" do
+    flunk "unimplemented"
+  end
+
+  test "banned cannot create comments" do
+    flunk "unimplemented"
+  end
+
+  test "admin can read comments" do
+    flunk "unimplemented"
+  end
+
+  test "moderator can read comments" do
+    flunk "unimplemented"
+  end
+
+  test "registered can read comments" do
+    flunk "unimplemented"
+  end
+
+  test "guest can read comments" do
+    flunk "unimplemented"
+  end
+
+  test "banned cannot read comments" do
+    flunk "unimplemented"
+  end
+
+  test "admin can destroy own comments" do
+    flunk "unimplemented"
+  end
+
+  test "admin can destroy other's comments" do
+    flunk "unimplemented"
+  end
+
+  test "moderator can destroy own comments" do
+    flunk "unimplemented"
+  end
+
+  test "moderator can destroy other's comments" do
+    flunk "unimplemented"
+  end
+
+  test "registered can destroy own comments" do
+    flunk "unimplemented"
+  end
+
+  test "registered cannot destroy other's comments" do
+    flunk "unimplemented"
+  end
+
+  test "guest cannot destroy comments" do
+    flunk "unimplemented"
+  end
+
+  test "banned cannot destroy comments" do
+    flunk "unimplemented"
+  end
 end
