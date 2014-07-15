b3784093d5ac890332548a524276dee8b8b531d1
Camden Narzt <camden.narzt@metabolistics.com>
2014-07-15 16:53:49 -0600

    More tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,24 +2,37 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @article = Article.find(params[:article_id])
-    params['comment']['user_id'] = current_user.id
-    @comment = @article.comments.create(comment_params)
-    if @comment.id.nil?
-      flash[:alert] = 'Comment '+ @comment.errors.messages[:body].first
+    grab_article
+    if can? :create, Comment
+      params['comment']['user_id'] = current_user.id
+      @comment = @article.comments.create(comment_params)
+      if @comment.id.nil?
+        flash[:alert] = 'Comment '+ @comment.errors.messages[:body].first
+      end
+    else
+      flash[:alert] = 'You are not allowed to do that.'
     end
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
+    grab_article
+    if can? :destroy, Comment
     @comment = @article.comments.find(params[:id])
-    @comment.destroy
+      if can? :destroy, @comment
+        @comment.destroy
+      else
+        flash[:alert] = 'You are not allowed to do that.'
+      end
+    end
     redirect_to article_path(@article)
   end
 
   private
   def comment_params
     params.require(:comment).permit(:body,:user_id)
+  end
+  def grab_article
+    @article = Article.find(params[:article_id])
   end
 end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -12,49 +12,70 @@ class CommentsControllerTest < ActionController::TestCase
     assert_redirected_to article_path(articles(:good).id)
   end
 
-#   test "moderator can create comments" do
-#     flunk "unimplemented"
-#   end
+  test "moderator can create comments" do
+    mod = users(:bob)
+    sign_in mod
 
-#   test "registered can create comments" do
-#     flunk "unimplemented"
-#   end
+    assert_difference('Comment.count',1) do
+      post :create, article_id: articles(:good).id, comment: {body: 'a long enough body'}
+    end
 
-#   test "guest cannot create comments" do
-#     flunk "unimplemented"
-#   end
+    assert_redirected_to article_path(articles(:good).id)
+  end
 
-#   test "banned cannot create comments" do
-#     flunk "unimplemented"
-#   end
+  test "registered can create comments" do
+    reg = users(:one)
+    sign_in reg
 
-#   test "admin can read comments" do
-#     flunk "unimplemented"
-#   end
+    assert_difference('Comment.count',1) do
+      post :create, article_id: articles(:good).id, comment: {body: 'a long enough body'}
+    end
 
-#   test "moderator can read comments" do
-#     flunk "unimplemented"
-#   end
+    assert_redirected_to article_path(articles(:good).id)
+  end
 
-#   test "registered can read comments" do
-#     flunk "unimplemented"
-#   end
+  test "guest cannot create comments" do
+    assert_no_difference('Comment.count') do
+      post :create, article_id: articles(:good).id, comment: {body: 'a long enough body'}
+    end
 
-#   test "guest can read comments" do
-#     flunk "unimplemented"
-#   end
+    assert_redirected_to new_user_session_path
+    assert_equal "You need to sign in or sign up before continuing.", flash[:alert]
+  end
 
-#   test "banned cannot read comments" do
-#     flunk "unimplemented"
-#   end
+  test "banned cannot create comments" do
+    ban = users(:bad)
+    sign_in ban
 
-#   test "admin can destroy own comments" do
-#     flunk "unimplemented"
-#   end
+    assert_no_difference('Comment.count') do
+      post :create, article_id: articles(:good).id, comment: {body: 'a long enough body'}
+    end
 
-#   test "admin can destroy other's comments" do
-#     flunk "unimplemented"
-#   end
+    assert_redirected_to article_path(articles(:good).id)
+    assert_not_equal "You need to sign in or sign up before continuing.", flash[:alert]
+  end
+
+  test "admin can destroy other's comments" do
+    admin = users(:ted)
+    sign_in admin
+
+    assert_difference('Comment.count',-1) do
+      delete :destroy, article_id: articles(:good).id, id: comments(:one).id
+    end
+
+    assert_redirected_to article_path(articles(:good).id)
+  end
+
+  test "admin can destroy own comments" do
+    admin = users(:ted)
+    sign_in admin
+
+    assert_difference('Comment.count',-1) do
+      delete :destroy, article_id: articles(:good).id, id: comments(:three).id
+    end
+
+    assert_redirected_to article_path(articles(:good).id)
+  end
 
 #   test "moderator can destroy own comments" do
 #     flunk "unimplemented"
