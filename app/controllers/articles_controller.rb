06a85042be7d166b3fef4028213be83b1309085f
Camden Narzt <camden.narzt@metabolistics.com>
2014-07-21 11:43:54 -0600

    Stub out auth/filtering delegation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
-
+#    load_and_authorize_resource
   def new
     if can? :create, Article
       @article = Article.new
