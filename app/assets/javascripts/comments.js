387f9203a2769462d0f7fa1b722dc1e6b9cc285c
Camden Narzt <camden.narzt@metabolistics.com>
2014-07-23 10:55:20 -0600

    Comments and keep alive tweaks

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+$( document ).ready(function() {
+  if($('div.panel-success').length > 0){
+    setTimeout(
+      function(){$('div.panel-success').removeClass('panel-success').addClass('panel-default');},
+      1000 * 5
+    );
+  }
+});
--- app/assets/javascripts/keepalive.js
+++ app/assets/javascripts/keepalive.js
@@ -1,6 +1,8 @@
-if($('a.dropdown-toggle').text().trim() !== 'Guest'){
-  setInterval(
-    function(){$.getJSON(document.location.origin+"/ping");},
-    1000 * 60 * (10 - 1)
-  );
-}
+$( document ).ready(function() {
+  if($('a.dropdown-toggle').text().trim() !== 'Guest'){
+    setInterval(
+      function(){$.getJSON(document.location.origin+"/ping");},
+      1000 * 60 * (10 - 1)
+    );
+  }
+});
