2862cebbba9d05a305efed49a083aa2cee51ddd2
Camden Narzt <camden.narzt@metabolistics.com>
2014-07-23 11:13:29 -0600

    select just the user dropdown

--- app/assets/javascripts/keepalive.js
+++ app/assets/javascripts/keepalive.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  if($('a.dropdown-toggle').text().trim() !== 'Guest'){
+  if($('#user a.dropdown-toggle').text().trim() !== 'Guest'){
     setInterval(
       function(){$.getJSON(document.location.origin+"/ping");},
       1000 * 60 * (10 - 1)
