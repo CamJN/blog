3b3dd8141b4d12f9dc7bb633eb4edc629904918b
Camden Narzt <camden.narzt@metabolistics.com>
2014-07-24 15:32:38 -0600

    Cleaned up overlay

--- app/assets/javascripts/overlay.js
+++ app/assets/javascripts/overlay.js
@@ -1,3 +1,3 @@
 $(document).on('ready page:change', function() {
-  $('#no-button').click(function(){$('#overlay').hide();});
+  $('#no-button').click(function(){$('#overlay').hide();$.cookie('no_tour',1);});
 });
