50174d1e20fe4403523dba8e505a2f4b5b16c507
Camden Narzt <admin@silversshadows.com>
2014-07-27 18:15:34 -0600

    Clean up ui

--- config/navigations/menu_navigation.rb
+++ config/navigations/menu_navigation.rb
@@ -5,11 +5,16 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.renderer = SimpleNavigation::Renderer::Bootstrap
 
   navigation.items do |primary|
+    if not Article.all.empty?
     primary.item :articles, 'Blog', articles_path do |sub_nav|
       Article.all.each do |a|
         sub_nav.item a.title.parameterize.underscore.to_sym, a.title, article_path(a)
       end
     end
+    else
+      primary.item :articles, 'Blog', articles_path
+    end
+
     primary.item :tour, 'Tour','#'
     primary.dom_class = "nav navbar-nav"
   end
