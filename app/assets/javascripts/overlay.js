2ec40bddd2916f1bcb6761f172c6a0139470e0b4
Camden Narzt <camden.narzt@metabolistics.com>
2014-08-01 15:50:19 -0600

    Make overlay responsive

--- app/assets/javascripts/overlay.js
+++ app/assets/javascripts/overlay.js
@@ -1,3 +1,10 @@
 $(document).on('ready page:change', function() {
+  if($('#overlay.phone').length > 0){
+    function resizefcn(){
+      $('#overlay.phone .col-'+(($(window).width() > $(window).height())?'sm':'xs')+'-6').toggleClass('col-sm-6').toggleClass('col-xs-6');
+    }
+    resizefcn();
+    $(window).resize(resizefcn);
+  }
   $('#no-button').click(function(){$('#overlay').hide();$.cookie('no_tour',1);});
 });
